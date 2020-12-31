6e782cc9f6febdb0113b3ac3075e53a8d7d7f282
Nash Luffman <nashluffman@gmail.com>
2020-12-30 22:43:39 -0500

    Cleaned up JSON response & added metadata

--- scrape.go
+++ scrape.go
@@ -1,8 +1,10 @@
-package scrape
+package main
 
 import (
 	"fmt"
 	"github.com/gocolly/colly/v2"
+	"log"
+	"regexp"
 	"strings"
 )
 
@@ -12,9 +14,9 @@ var (
 
 type Trail struct {
 	Name   string `json:"trail"`
-	Link   string `json:"trail_info"`
-	Date   string `json:"last_updated"`
 	Status string `json:"status"`
+	Date   string `json:"last_updated"`
+	Link   string `json:"trail_info"`
 }
 
 func newTrail(e *colly.HTMLElement) Trail {
@@ -22,35 +24,47 @@ func newTrail(e *colly.HTMLElement) Trail {
 		name string
 		status string
 		date string
+		link string
 		elements []string
 	)
 
+	// Maybe colly has some native support for what I'm doing, but I couldn't find it
 	e.ForEach("td", func(_ int, td *colly.HTMLElement){
 		elements = append(elements, td.Text)
 	})
 
+	// All trails follow the same order of name, status, and date
 	name = elements[0]
 	status = strings.ToLower(elements[1])
 	date = elements[2]
 
 	page := e.ChildAttr("a", "href")
-	link := fmt.Sprintf("%s%s", "https://www.trianglemtb.com/", page)
+	link = fmt.Sprintf("%s%s", "https://www.trianglemtb.com/", page)
 
+	// Some of the trails have a weird character thrown in as they are sub-trails, but we just treat them as normal
+	re, err := regexp.Compile(`[^A-Za-z0-9 - ( )]`)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	name = re.ReplaceAllString(name, " ")
 
 	t := Trail{
 		Date:   date,
 		Name:   strings.TrimSpace(name),
 		Link:   link,
-		Status: status,
+		Status: strings.TrimSpace(status),
 	}
 	return t
 }
 
-func FetchTrails() []Trail {
+func FetchTrailStatus() []Trail {
 	c := colly.NewCollector()
 
-	// On every a element which has href attribute call callback
+	// On every a element which has tr attribute call callback
 	c.OnHTML("tr", func(e *colly.HTMLElement) {
+
 		// For trails with comments, they have an id attribute on the tr element
 		// Since we don't serve the comments as a part of this API, we just skip over it :)
 		if len(e.Attr("id")) > 0 {
--- serve.go
+++ serve.go
@@ -5,14 +5,46 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"trails-api/scrape"
+	"time"
 )
 
+type metadata struct {
+	Timestamp string `json:"query_time"`
+}
+
+type response struct {
+	Metadata metadata `json:"metadata"`
+	Data []Trail  `json:"data"`
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	marshaledTrails, _ := json.Marshal(scrape.FetchTrails())
+	log.Printf("%s connected with %s", r.RemoteAddr, r.Header.Get("User-Agent"))
+
+	var (
+		trails []Trail
+		responseMetadata metadata
+		finalResponse response
+	)
+
+	trails = FetchTrailStatus()
+
+	responseMetadata = metadata {
+		Timestamp: time.Now().Format("2006.01.02 15:04:05"),
+	}
+
+	finalResponse = response {
+		Metadata: responseMetadata,
+		Data:     trails,
+	}
+
+	marshaledResponse, err := json.MarshalIndent(finalResponse, "", "  ")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	w.Header().Set("Content-Type", "application/json")
-	w.Write(marshaledTrails)
+	w.Write(marshaledResponse)
 }
 
 func main() {
