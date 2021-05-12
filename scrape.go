3c964e87c9dd8e12cbf65dafdac124559e0f3797
Nash Luffman <nashluffman@gmail.com>
2021-05-11 23:24:11 -0400

    Adds support for trail comments

--- scrape.go
+++ scrape.go
@@ -11,11 +11,12 @@ import (
 type trail struct {
 	Name   string `json:"trail"`
 	Status string `json:"status"`
+	Notes string `json:"notes"`
 	Date   string `json:"last_updated"`
 	Link   string `json:"trail_info"`
 }
 
-func NewTrail(name string, status string, date string, link string) trail {
+func NewTrail(name string, status string, date string, link string, notes string) trail {
 
 	// Some of the trails have a weird character thrown in as they are sub-trails, but we just treat them as normal
 	re, err := regexp.Compile(`[^A-Za-z0-9 - ( )]`)
@@ -30,29 +31,31 @@ func NewTrail(name string, status string, date string, link string) trail {
 	t := trail{
 		Date:   date,
 		Name:   strings.TrimSpace(name),
+		Notes: notes,
 		Link:   link,
 		Status: strings.TrimSpace(status),
 	}
 	return t
 }
 
-func parseColly(e *colly.HTMLElement) []string{
-
-	var (
-		results []string
-	)
-
+func parseColly(e *colly.HTMLElement) (details []string){
+	var note string
 	// Maybe colly has some native support for what I'm doing, but I couldn't find it
 	e.ForEach("td", func(_ int, td *colly.HTMLElement){
-		results = append(results, td.Text)
+		nAttr := td.Attr("onclick")
+		if len(nAttr) > 0 {
+			note = nAttr[strings.Index(nAttr, "'") + 1 : strings.Index(nAttr, ",") - 1]
+		}
+		details = append(details, td.Text)
 	})
 
 
 	page := e.ChildAttr("a", "href")
 	link := fmt.Sprintf("%s%s", "https://www.trianglemtb.com/", page)
 
-	results = append(results, link)
-	return results
+	details = append(details, link)
+	details = append(details, note)
+	return details
 }
 
 func FetchTrailStatus() []trail {
@@ -60,13 +63,21 @@ func FetchTrailStatus() []trail {
 		trails []trail
 	)
 
+	notes := make(map[string]string)
 	c := colly.NewCollector()
+
+	// It's not pretty - but we need to build up the map of all notes before
+	// we build out each trail object
+	c.OnHTML("tr", func(e *colly.HTMLElement){
+		if len(e.Attr("id")) > 0 {
+			notes[e.Attr("id")] = e.Text
+		}
+	})
 
 	// On every a element which has tr attribute call callback
 	c.OnHTML("tr", func(e *colly.HTMLElement) {
 
-		// For trails with comments, they have an id attribute on the tr element
-		// Since we don't serve the comments as a part of this API, we just skip over it :)
+		// tr elements with an id have already been handled
 		if len(e.Attr("id")) > 0 {
 			return
 		}
@@ -77,8 +88,7 @@ func FetchTrailStatus() []trail {
 		}
 
 		elements := parseColly(e)
-		currentTrail := NewTrail(elements[0], elements[1], elements[2], elements[3])
-
+		currentTrail := NewTrail(elements[0], elements[1], elements[2], elements[3], notes[elements[4]])
 		trails = append(trails, currentTrail)
 	})
 
